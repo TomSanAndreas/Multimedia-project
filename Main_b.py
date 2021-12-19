ba3c992a1eca7339433b32807d4a001fb0bf312b
hvbladel <han.vanbladel@ugent.be>
2021-12-19 21:23:35 +0100

    update slides + main_b zonder muziekdeel

---
+++ Main_b.py
@@ -0,0 +1,26 @@
+# Algemene imports
+import cv2
+from time import sleep
+
+# Eigen imports
+from Solver.base import Solver
+from Solver.types import Types
+from Solver.utils import *
+
+# main functie
+if __name__ == "__main__":
+    # bestandsnaam opvragen
+    filename = input("Geef de naam van het bestand op (standaard: \"tiles_rotated_2x2_00\"): ")
+    if len(filename) == 0:
+        filename = "tiles_rotated_2x2_00"
+    # bestandsnaam proberen omzetten naar een geldig pad
+    filename = get_image_path(filename)
+    if filename is None:
+        print("Bestand werd niet gevonden!")
+    else:
+        type_puzzle = get_file_info_by_name(filename)
+        src_image = cv2.imread(filename)
+        solver = Solver(src_image, type_puzzle)
+        solver.solve()
+
+        solver.show()
